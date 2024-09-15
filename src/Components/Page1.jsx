1579ab64a089b411a49da676e22258afd16f4694
IkedaLab Daniel <callejasmark63@gmail.com>
2024-09-15 13:52:36 +0800

    Added class room. Added clipboard copy when phone number is clicked

--- src/Components/Page1.jsx
+++ src/Components/Page1.jsx
@@ -6,28 +6,38 @@ function Page1(){
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const dayName = daysOfWeek[dayIndex];
 
+    const textToCopy = "09051315374";
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(textToCopy).then(() => {
+          alert("Text copied to clipboard!");
+        }).catch((err) => {
+          console.error("Failed to copy text: ", err);
+        });
+    }
+
     const [handleAnswer, setHandleAnswer] = useState(<p>Ask a question!</p>);
     const classes = [
-        <p key={3} className='class'>❤️No Class</p>,
-        <p key={4}  className='class'>👨‍🏫 CISCO 9:00am - 12:00pm <span className='yellow-side'>F2F</span><br/><br/><span className='async'>📚 Asyncronous: Database, Data Structure</span></p>,
+        <p key={3} className='class'>❤️ No Class</p>,
+        <p key={4}  className='class'>👨‍🏫 CISCO 9:00am - 12:00pm <span className='yellow-side'>F2F @ CISCO Lab</span><br/><br/><span className='async'>📚 Asyncronous: Database, Data Structure</span></p>,
 
         <p key={5}  className='class'>👨‍🏫 Accounting 3:00pm - 6:00pm  <span className='yellow-side'>Online</span></p>,
 
         <p key={6}  className='class'>
-            👨‍🏫Database 8:00am - 11:00am <span className='yellow-side'>F2F</span> <br/>
+            👨‍🏫Database 8:00am - 11:00am <span className='yellow-side'>F2F @ Red Lab</span> <br/>
             👨‍🏫Science 6:00pm - 9:00pm <span className='yellow-side'>Online</span>  <br/><br/>
             <span className='async'>📚 Asyncronous: Understanding the Self</span>
             </p>,
 
-        <p key={7}  className='class'>👨‍🏫Data Structure 12:00pm - 3:00pm <span className='yellow-side'>F2F</span> <br/><br/>
+        <p key={7}  className='class'>👨‍🏫Data Structure 12:00pm - 3:00pm <span className='yellow-side'>F2F @ iLab</span> <br/><br/>
             <span className='async'>📚 Asyncronous: Art Appreciation, CISCO</span>
         </p>,
 
-        <p key={8}  className='class'>👨‍🏫PE3 10:00am - 12:00pm <span className='yellow-side'>F2F</span> <br/>
-            👨‍🏫Web Programming 2:00pm - 5:00pm <span className='yellow-side'>F2F</span></p>,
+        <p key={8}  className='class'>👨‍🏫PE3 10:00am - 12:00pm <span className='yellow-side'>F2F @ Gymnasium (Main)</span> <br/>
+            👨‍🏫Web Programming 2:00pm - 5:00pm <span className='yellow-side'>F2F @ iLab/CISCO Lab</span></p>,
 
-        <p key={9}  className='class'>❤️No Class<br/><br/><span className='async'>Asyncronous:<br/> WebProg 11AM - 1PM</span></p>,
-        <p key={3} className='class'>❤️No Class</p>,
+        <p key={9}  className='class'>❤️ No Class<br/><br/><span className='async'>Asyncronous:<br/> WebProg 11AM - 1PM</span></p>,
+        <p key={3} className='class'>❤️ No Class</p>,
     ];
 
     const Tasks = <p key="1">📌 CISCO netacad Module 1-2 <span className='yellow-side'>Self-paced</span>  <br/><br/>
@@ -39,10 +49,14 @@ function Page1(){
     const toPay = <p key="2">📌 IBCE Student Activity Fee:<br/> ₱150 <span className='yellow-side'>DUE Sep.18</span> <br/><br/>
                     📌 SSITE Membership Fee: ₱200<br/><br/>
                     💵 Pay on: <br/>
-                    GCash - 09051315374<br/>
+                    GCash - <span className="phone-num" onClick={copyToClipboard}>{textToCopy} </span><br/>
+                    <span className='yellow-side'>Click number to copy</span><br/>
+                    (J**R C***S)<br/>
+                    <br/>
                     Cash - See Ice during F2F class</p>
 
     const classToday = classes[dayIndex];
+
 
     function showAnswer1(){
         setHandleAnswer(prevState => (classToday));
@@ -60,6 +74,7 @@ function Page1(){
 
     function showAnswer4(){
         setHandleAnswer(prevState => (classes[(dayIndex + 1)]));
+        console.log(dayIndex + 1)
     }
 
 
