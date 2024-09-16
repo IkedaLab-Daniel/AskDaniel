17a50b65c267a5ca17a65efca6ce4fe086094e77
IkedaLab Daniel <callejasmark63@gmail.com>
2024-09-16 14:36:19 +0800

    added animation to Files

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     return(
       <>
         <Page1 />
-        <span className='version'>Ver 1.1.1</span>
+        <span className='version'>Ver 1.1.2</span>
         <Footer />
         
       </>
