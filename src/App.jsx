f569b8759ceb6102e5443002c0770c235a5f7d74
Daniel <callejasmark63@gmail.com>
2024-11-09 23:13:59 +0800

    Changes in Modalities

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     return(
       <>
         <Page1 />
-        <span className='version'>V1.4.6</span>
+        <span className='version'>V1.4.7</span>
         <Footer />
       </>
     )
