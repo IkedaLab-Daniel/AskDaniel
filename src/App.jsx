483d4ea84404230227c7fd50818cb8a0b5e2ce2d
Daniel <callejasmark63@gmail.com>
2024-12-01 10:58:38 +0800

    Version number typo

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     return(
       <>
         <Page1 />
-        <span className='version'>V1.4.14</span>
+        <span className='version'>V1.4.15</span>
         <Footer />
       </>
     )
