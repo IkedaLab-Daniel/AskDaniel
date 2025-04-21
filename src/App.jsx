3a6e6e8597079fdc4b4f6b003756398d527d2b6d
Daniel <callejasmark63@gmail.com>
2025-04-21 20:11:18 +0800

    ❄️ Corrected version number

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     return(
       <>
         <Page1 />
-        <span className='version'>V1.6.10</span>
+        <span className='version'>V1.6.11</span>
         <Footer />
       </>
     )
